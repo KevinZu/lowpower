f534da3c2169407d3308896587ddcac460f501e2
zukeqiang <zukeqiang@voyagerauto.com>
2020-03-31 09:21:04 +0000

    wake lock

--- include/WakeLock.h
+++ include/WakeLock.h
@@ -1,7 +1,34 @@
 #ifndef __WAKE_LOCK_H__
 #define __WAKE_LOCK_H__
 
+#include <stdint.h>
+#include <string>
+
+
 namespace VoyagerAuto {
+
+enum {
+	ACQUIRE_PARTIAL_WAKE_LOCK = 0,
+	RELEASE_WAKE_LOCK,
+	OUR_FD_COUNT
+};
+
+enum {
+	PARTIAL_WAKE_LOCK = 1,  // the cpu stays on, but the screen is off
+	FULL_WAKE_LOCK = 2      // the screen is also on
+};
+
+#if __cplusplus
+extern "C" {
+#endif
+
+// while you have a lock held, the device will stay on at least at the
+// level you request.
+int acquire_wake_lock(int lock, const char* id);
+int release_wake_lock(const char* id);
+#if __cplusplus
+} // extern "C"
+#endif
 
 class WakeLock {
 	WakeLock();
@@ -11,6 +38,12 @@ public:
 
 	int Lock();
 	int UnLock(int);
+private:
+	int AcquireWakeLock();
+	int ReleaseWakeLock();
+	int m_fds[OUR_FD_COUNT];
+	std::string m_id;
+	int m_lock_count;
 };
 
 }
--- src/WakeLock.cpp
+++ src/WakeLock.cpp
@@ -1,18 +1,71 @@
 #include <iostream>
 #include "WakeLock.h"
 
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <pthread.h>
+
 using namespace VoyagerAuto;
 using namespace std;
 //////////////////////////////////////////////////////////
 
-WakeLock::WakeLock()
-{
-	cout << "+ wake lock\n";
+const char * const NEW_PATHS[] = {
+	"/sys/power/wake_lock",
+	"/sys/power/wake_unlock",
+};
+
+
+#define MY_ID  "ag35lock"
+
+
+
+
+int	WakeLock::AcquireWakeLock()
+{
+	int fd;
+	fd = m_fds[ACQUIRE_PARTIAL_WAKE_LOCK];
+	return write(fd, m_id.c_str(), strlen(m_id.c_str()));
+}
+int WakeLock::ReleaseWakeLock()
+{
+	ssize_t len = write(m_fds[RELEASE_WAKE_LOCK], m_id.c_str(), strlen(m_id.c_str()));
+	return len >= 0;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+WakeLock::WakeLock() : m_id(MY_ID),m_lock_count(0)
+{
+	cout << "+ wake lock  my_id:" << m_id << endl;
+	int i;
+	for (i=0; i<OUR_FD_COUNT; i++) {
+		int fd = open(NEW_PATHS[i], O_RDWR | O_CLOEXEC);
+		if (fd < 0) {
+			printf("fatal error opening \"%s\"\n", NEW_PATHS[i]);
+		}
+		m_fds[i] = fd;
+	}
+
+	
 }
 
 WakeLock::~WakeLock()
 {
 	cout << "- wake lock\n";
+	int i;
+	for (i=0; i<OUR_FD_COUNT; i++) {
+		if(m_fds[i] > 0)
+			close(m_fds[i]);
+	}
 }
 
 WakeLock& WakeLock::GetInstance()
@@ -24,12 +77,34 @@ WakeLock& WakeLock::GetInstance()
 
 int WakeLock::Lock()
 {
-	return 0;
+	if(m_lock_count < 0) {
+		cout << "sys lock is failure!" << endl;
+		return -1;
+	}
+	
+	if(m_lock_count == 0) {
+		AcquireWakeLock();
+	}
+
+	m_lock_count += 1;
+	
+	return m_lock_count;
 }
 
 
 int WakeLock::UnLock(int lockId)
 {
-	return 0;
+	if (m_lock_count <= 0) {
+		cout << "lock is already releaseWakeLock!" << endl;
+		return -1;
+	}
+	
+	if (m_lock_count == 1) {
+		ReleaseWakeLock();
+	}
+
+	m_lock_count -= 1;
+	
+	return m_lock_count;
 }
 
